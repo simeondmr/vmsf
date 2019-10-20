ff8aca95ce01431608c93fda26e6a06285bc1270
Simeon <simeon@localhost.localdomain>
2019-10-20 20:32:10 +0200

    begin draft

---
+++ src/vm/vm.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "../include/opcodes.h"
+
+int execute(unsigned char *code) {
+  
+/*	while (NOT END) {
+		//opcode = fetch(code)
+		switch(opcode) {
+		case NOP:
+			break;
+   
+	  
+		}
+	}
+*/
+  return 0;
+}
