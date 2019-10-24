1231ca938a74671cf143ad286f5795dc4b471f94
Simeon <simeon@localhost.localdomain>
2019-10-24 20:58:38 +0200

    fix ram.h

--- src/include/ram.h
+++ src/include/ram.h
@@ -1,6 +1,6 @@
 #ifndef __RAM_H
 #define __RAM_H
-
+#include <stdint.h>
 uint8_t *setup_ram();
 void unsetup_ram(uint8_t *ram);
 void write_ram(uint8_t *ram, uint32_t addr, uint8_t *data, uint32_t size);
